30bafd3c71a266b652e574d23077ee473cf1cd9f
unknown <penheadcreative@gmail.com>
2016-05-27 10:55:41 -0400

    removed fadetoggle and added custom swap in and out for hamburger to x icon for menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+var menuToggle = false;
 $( "#menu" ).click(function() {
+	menuToggle = !menuToggle;
+	if(menuToggle == true){
+		$(".links").fadeIn();
+			$(".box-shadow-menu").addClass("box-shadow-menu-x");
+
+	}else{
+		$(".links").fadeOut();
+			$(".box-shadow-menu").removeClass("box-shadow-menu-x");
+
+	}
 	
-	console.log('afdsfadsfdsf');
-	//$(".links").fadeIn();
-	$(".links").fadeToggle();
 });
