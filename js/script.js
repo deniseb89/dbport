51c2eb8d1ce559b2c44de6699eabc4f046503b99
unknown <penheadcreative@gmail.com>
2016-05-27 10:56:37 -0400

    code clean up

--- js/script.js
+++ js/script.js
@@ -3,12 +3,9 @@ $( "#menu" ).click(function() {
 	menuToggle = !menuToggle;
 	if(menuToggle == true){
 		$(".links").fadeIn();
-			$(".box-shadow-menu").addClass("box-shadow-menu-x");
-
+		$(".box-shadow-menu").addClass("box-shadow-menu-x");
 	}else{
 		$(".links").fadeOut();
-			$(".box-shadow-menu").removeClass("box-shadow-menu-x");
-
+		$(".box-shadow-menu").removeClass("box-shadow-menu-x");
 	}
-	
 });
