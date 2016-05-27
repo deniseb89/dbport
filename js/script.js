85b0fda6e2150c4419fbd6987bc1d50a9c0defb1
unknown <penheadcreative@gmail.com>
2016-05-27 15:28:02 -0400

    changes to style

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ $( "#menu" ).click(function() {
 	menuToggle = !menuToggle;
 	if(menuToggle == true){
 		$(".links").fadeIn();
+		$("body").css("overflow" ,"hidden")
 		$(".box-shadow-menu").addClass("box-shadow-menu-x");
 	}else{
 		$(".links").fadeOut();
+		$("body").css("overflow" ,"scroll")
 		$(".box-shadow-menu").removeClass("box-shadow-menu-x");
 	}
 });
