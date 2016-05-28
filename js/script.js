bb95acf52b5511d892afcc30fd7ff1cd9b583855
Penhead <penheadcreative@gmail.com>
2016-05-27 20:34:54 -0400

    remove commit mistake

--- js/script.js
+++ js/script.js
@@ -4,18 +4,8 @@ $( "#menu" ).click(function() {
 	if(menuToggle == true){
 		$('#menu').addClass('is-active');
 		$(".links").fadeIn();
-<<<<<<< HEAD
-		$("body").css("overflow" ,"hidden")
-		$(".box-shadow-menu").addClass("box-shadow-menu-x");
-=======
->>>>>>> 43bb6623fb29ecd8c52cfdd9a6c4c046e16809a1
 	}else{
 		$('#menu').removeClass('is-active');
 		$(".links").fadeOut();
-<<<<<<< HEAD
-		$("body").css("overflow" ,"scroll")
-		$(".box-shadow-menu").removeClass("box-shadow-menu-x");
-=======
->>>>>>> 43bb6623fb29ecd8c52cfdd9a6c4c046e16809a1
 	}
 });
