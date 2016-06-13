be0a8bcd432d0106a118a080f77fcd893156b04d
deniseb89 <denise_b89@hotmail.com>
2016-06-12 20:48:08 -0400

    sprinkled in some angular

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('app', ["ui.router"]);
+
+app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise("/");
+    $stateProvider
+        .state('home', {
+            url: "/",
+            controller: "HomeController",
+            templateUrl: "views/home.html"
+        })
+        .state('portfolio', {
+            url: "/portfolio",
+            controller: "PortfolioController",
+            templateUrl: "views/portfolio.html"
+        })
+}]);
