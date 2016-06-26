8934d55f643240ae5d2eb845b7f26dba80842b0f
deniseb89 <denise_b89@hotmail.com>
2016-06-26 10:01:58 -0400

    the blog works!

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,5 +1,11 @@
 app.controller('BlogController', function($scope, $http, $sce) {
     $http.get("posts.json").then(function(response) {
         $scope.posts = response.data;
+        angular.forEach($scope.posts, function(post) {
+            $http.get(post.file).then(function(response) {
+                post.file = markdown.toHTML(response.data);
+                $sce.trustAsHtml(post.file);
+            });
+        });
     });
 });
